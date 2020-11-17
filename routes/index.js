ae0b5b3d51a1c730edc6301ce4e998627612a2db
Gilbert Emerson <emer7@users.noreply.github.com>
2020-11-17 23:04:18 +0800

    Basic functionalities

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,65 @@
 var express = require('express');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const groupChatsStore = {};
 
-module.exports = router;
+module.exports = whatsAppClient => {
+  router.get('/', async (req, res, next) => {
+    const chats = await whatsAppClient.getChats();
+
+    const groupChats = chats.filter(chat => chat.isGroup);
+    groupChats.forEach(groupChat => {
+      groupChatsStore[groupChat.name] = groupChat;
+    });
+
+    res.render('chats', { groupChats });
+  });
+
+  router.get('/group/:groupName', async (req, res) => {
+    const { groupName } = req.params;
+
+    const groupChat = groupChatsStore[groupName];
+    const { participants } = groupChat;
+
+    const contacts = (
+      await Promise.all(
+        participants.map(participant =>
+          whatsAppClient.getContactById(participant.id._serialized)
+        )
+      )
+    ).filter(contact => !contact.isMe);
+
+    res.render('participants', {
+      participants: contacts,
+    });
+  });
+
+  router.post('/group/:groupName', async (req, res) => {
+    const { groupName } = req.params;
+
+    const groupChat = groupChatsStore[groupName];
+    const { participants } = groupChat;
+
+    const contacts = (
+      await Promise.all(
+        participants.map(participant =>
+          whatsAppClient.getContactById(participant.id._serialized)
+        )
+      )
+    ).filter(contact => !contact.isMe);
+
+    await Promise.all(
+      contacts.map(contact => {
+        whatsAppClient.sendMessage(
+          contact.id._serialized,
+          'Final hard coded message'
+        );
+      })
+    );
+
+    res.send('ok');
+  });
+
+  return router;
+};
